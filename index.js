d5f2c373e9e74e7752fb70ff64f811e0f06cf67a
JoshuaGottus <joshmg426@gmail.com>
2023-03-15 05:15:41 +0100

    Hero Section

--- index.js
+++ index.js
@@ -37,4 +37,34 @@ const showMenu = (toggleId, navbarId, bodyId)=>{
     })
   }
   
+
+  /* -- Text effect -- */
+
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let interval = null;
   
+  document.querySelector(".hero-hover").onmouseover = event => {  
+    let iteration = 0;
+    
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if(index < iteration) {
+            return event.target.dataset.value[index];
+          }
+        
+          return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+      
+      if(iteration >= event.target.dataset.value.length){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 30);
+  }
